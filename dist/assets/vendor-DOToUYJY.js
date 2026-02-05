adbf07b72c6d5847205ae5f9af09874b877137b0
Usman Eletu <usmaneletu2@gmail.com>
2026-02-05 13:37:16 +0100

    aut

---
+++ dist/assets/vendor-DOToUYJY.js
@@ -0,0 +1,1 @@
+import"./router-PqDyvGf0.js";
--- src/pages/VerifyPasswordOtpPage.tsx
+++ src/pages/VerifyPasswordOtpPage.tsx
@@ -81,7 +81,9 @@ export default function VerifyPasswordOtp() {
                   {otp.map((digit, index) => (
                     <input
                       key={index}
-                      ref={(el) => (inputRefs.current[index] = el)}
+                      ref={(el) => {
+                        inputRefs.current[index] = el;
+                      }}
                       type="text"
                       inputMode="numeric"
                       pattern="[0-9]*"
